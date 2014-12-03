39047df5f8fd52486748bdd065cab1ee6bfa87e4
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-02 19:12:09 -0800

    Successful signin

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,13 +2,13 @@ class Provider < ActiveRecord::Base
   belongs_to :user
 
   def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+    where(name: auth["provider"], uid: auth["uid"], name: auth["info"]["name"]).first || create_from_omniauth(auth)
   end
 
   def self.create_from_omniauth(auth)
     user = User.create #this should be find user method or create
     create! do |provider|
-      provider.provider = auth["provider"]
+      provider.name = auth["provider"]
       provider.uid = auth["uid"]
       provider.name = auth["info"]["name"]
       provider.user_id = user.id

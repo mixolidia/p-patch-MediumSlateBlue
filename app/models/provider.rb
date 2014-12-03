a608d11338b33cd20b878660ac15454d020161c6
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-02 18:51:45 -0800

    Add provider find/create with omniauth

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,16 @@
 class Provider < ActiveRecord::Base
+
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    user = User.create #this should be find user method or create
+    create! do |provider|
+      provider.provider = auth["provider"]
+      provider.uid = auth["uid"]
+      provider.name = auth["info"]["name"]
+      provider.user_id = user.id
+    end
+  end
 end

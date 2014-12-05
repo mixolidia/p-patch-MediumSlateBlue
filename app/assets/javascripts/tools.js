80dd12a550eab57a5a7e1ef8b3795ed18a32924f
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-05 10:30:55 -0800

    Now the fields are populating in the right place

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -9,11 +9,11 @@ $(function() {
 
   $("#add_field").click(function(event) {
     event.preventDefault();
-    var $emptyField = $('<form accept-charset="UTF-8" action="/tools/add" class="new_tool" id="new_tool" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="LOpwmfCiuYBU1UaM48ebgs0uaah5ya3rrQY8ijDyRtE="></div><div><input id="tool_name" name="tool[name]" placeholder="Tool" type="text"></div></form>');
-    $("#input_field").append($emptyField);
+    var $emptyField = $('<div><input id="tool_name" name="tool[name]" placeholder="Tool" type="text"><a href="#" class="remove_field">Remove</a></div>');
+    $(".input_field form").append($emptyField);
   });
 
-  $("#input_field").on("click",".remove_field", function(event){ //user click on remove text
+  $(".input_field").on("click",".remove_field", function(event){ //user click on remove text
     event.preventDefault(); $(this).parent('div').remove();
   });
 

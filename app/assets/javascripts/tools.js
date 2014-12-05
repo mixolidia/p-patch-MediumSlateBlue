8181fa6480eef741160dc5d87521bcaac7608a1e
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-05 09:16:55 -0800

    Append is working. Submit button is not.

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -5,21 +5,32 @@
 //TOOLS
 //manage.html.erb, append input field
 
-$(document).ready(function() {
-  var max_fields      = 10; //maximum input boxes allowed
-  var wrapper         = $("#input_field"); //Fields wrapper
-  var add_button      = $("#add_field"); //Add button ID
-
-  var x = 1; //initlal text box count
-  $(add_button).click(function(event){ //on add input button click
+$(function() {
+  $("#add_field").click(function(event) {
     event.preventDefault();
-    if(x < max_fields){ //max input box allowed
-      x++; //text box increment
-      $(wrapper).append("<%=f.text_field :name, placeholder: 'Tool' %>"); //add input box
-    }
+    var $emptyField = $("<div><input id='tool_name' name='tool[name]' placeholder='Tool' type='text'></div>");
+    $("#input_field").append($emptyField);
   });
 
-  $(wrapper).on("click","#remove_field", function(event){ //user click on remove text
-    event.preventDefault(); $(this).parent('div').remove(); x--;
-  });
 });
+
+
+
+// $(document).ready(function() {
+//   var max_fields      = 10; //maximum input boxes allowed
+//   var wrapper         = $("#input_field"); //Fields wrapper
+//   var add_button      = $("#add_field"); //Add button ID
+//
+//   var x = 1; //initlal text box count
+//   add_button.click(function(event){ //on add input button click
+//     event.preventDefault();
+//     if(x < max_fields){ //max input box allowed
+//       x++; //text box increment
+//       $(wrapper).append("<%=f.text_field :name, placeholder: 'Tool' %>"); //add input box
+//     }
+//   });
+//
+//   $(wrapper).on("click","#remove_field", function(event){ //user click on remove text
+//     event.preventDefault(); $(this).parent('div').remove(); x--;
+//   });
+// });

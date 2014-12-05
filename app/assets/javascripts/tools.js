e3504acc71da9a8922e4fb67c53d5ab3616bcb9a
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-05 09:52:01 -0800

    Just minor spacing changes

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -6,12 +6,15 @@
 //manage.html.erb, append input field
 
 $(function() {
+
   $("#add_field").click(function(event) {
     event.preventDefault();
-    var $emptyField = $("<div><input id='tool_name' name='tool[name]' placeholder='Tool' type='text'><a href='#' class='remove_field'>Remove</a></div>");
+    var $emptyField = $('<form accept-charset="UTF-8" action="/tools/add" class="new_tool" id="new_tool" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="LOpwmfCiuYBU1UaM48ebgs0uaah5ya3rrQY8ijDyRtE="></div><div><input id="tool_name" name="tool[name]" placeholder="Tool" type="text"></div></form>');
     $("#input_field").append($emptyField);
   });
+
   $("#input_field").on("click",".remove_field", function(event){ //user click on remove text
     event.preventDefault(); $(this).parent('div').remove();
   });
+
 });

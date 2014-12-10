23fe4c413c08610fb810f6425de36a79031a638b
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-10 15:51:34 -0800

    Work on tool email reminder

--- app/jobs/toolreminder.rb
+++ app/jobs/toolreminder.rb
@@ -2,7 +2,7 @@ class ToolReminder
   @queue = :tool
 
   def self.perform(tool_id)
-    tool = Tool.find(tool_id)
-    UserMailer.tool_reminder(unavailable_tool).deliver
+    due_tool = Tool.find(tool_id)
+    UserMailer.tool_reminder(due_tool).deliver
   end
 end

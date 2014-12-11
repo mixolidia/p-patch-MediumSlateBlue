5dc4d4618a894c1a0946ad63ee12dbc0f0fd67e1
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-10 16:33:44 -0800

    Rename job file, add resque to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
+  mount Resque::Server, :at => "/resque"
 
   root 'home#index'
   get  'auth/twitter/callback',       to: 'sessions#create'

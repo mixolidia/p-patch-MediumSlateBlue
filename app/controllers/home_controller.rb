b96e59a30ddcd6522c9c163c949964e2ad6587b7
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-11 13:29:16 -0800

    Start on recent news posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @posts = Post.recent_five
+    #@events = Event.recent_five
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   validates_presence_of :title, :content
   validates :user_id, presence: true
 
+  def self.all_by_date
+    all.sort { |a, b| a.created_at <=> b.created_at }
+  end
+
+  def self.recent_five
+    all_by_date[0..4]
+  end
 end
